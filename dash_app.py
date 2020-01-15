d21326ae2abe2b8e79d4e28e2af8a5cf51c30812
DavidSilva98 <david.silva.silvestre@gmail.com>
2020-01-15 02:54:31 +0000

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -21,9 +21,10 @@ from pandas import DataFrame
 
 df = pd.read_csv("./data/final_df.csv")
 
-
-
 # ----------------------------------------------------- FIGURES --------------------------------------------------------
+# TODO: Change font color in each figure for white
+# TODO: Change pad between figures
+
 fig_map = go.Figure(
     data=go.Scattermapbox(
         lat=df["latitude"],
@@ -86,7 +87,7 @@ fig_hist = go.Figure(
                                   args=['xbins.size', i]) for i in range(1, 20)]
                       )
                  ]
-        )
+    )
 )
 
 fig_hist.data[0].marker.line = dict(color='black', width=2)
@@ -107,19 +108,25 @@ app = dash.Dash(__name__)
 # ------------------------------------------------------- HTML
 # Layout of Dash App
 app.layout = html.Div(
+    id="div-universe",
     children=[
         html.Div(
             # TOP ROW
+            id="div-header",
             className="row",
             children=[
                 html.Div(
+                    id="div-header-1",
                     className="two columns div-user-controls",
                     children=[
                         html.Img(
-                            className="logo", src=app.get_asset_url("airbnb_logo.png")
+                            id="logo-image",
+                            className="logo",
+                            src=app.get_asset_url("airbnb_logo.png")
                         ),
-                        html.H3("AIRBNB APP"),
+                        html.H3("AIRBNB APP", id="title"),
                         dcc.Markdown(
+                            id="source",
                             children=[
                                 "Source: [Inside Airbnb](http://insideairbnb.com/get-the-data.html)"
                             ]
@@ -127,30 +134,35 @@ app.layout = html.Div(
                     ]
                 ),
                 html.Div(
+                    id="div-header-2",
                     className="four columns div-user-controls",
                     children=[
-                        "The following application describes the airbnb listings of Lisbon."
-                    ]
+                        """The following application describes the Airbnb listings of Lisbon.
+                        This dashboard is fully interactive and can be used to choose the ideal place to stay in Lisbon.
+                         """
+                    ]  # TODO: Escrever melhor descrição
                 ),
                 html.Div(
+                    id="div-header-3",
                     className="three columns div-user-controls",
                     children=[
                         "Interact with the dashboard: ",
                         html.Div(
+                            id="div-dropdown-1",
                             className="div-for-dropdown",
                             children=[
                                 # Dropdown for locations on map
                                 dcc.Dropdown(
                                     id='dcc_neighbourhood_dropdown',
                                     options=[{'label': i, 'value': i} for i in
-                                        ["All"] + df["neighbourhood_group_cleansed"].unique().tolist()],
-                                    value=None,
+                                             ["All"] + df["neighbourhood_group_cleansed"].unique().tolist()],
                                     placeholder="Select Municipality",
                                     style={'max-width': '250px'}
                                 )
                             ]
                         ),
                         html.Div(
+                            id="div-dropdown-2",
                             className="div-for-dropdown",
                             children=[
                                 # Dropdown to select variables
@@ -160,7 +172,6 @@ app.layout = html.Div(
                                         ["Availability", "Superhost", "Property Type", "Cancellation Policy"],
                                         ["availability_next_30", "host_is_superhost", "property_type",
                                          "cancellation_policy"])],
-                                    value=None,
                                     placeholder="Select Variable",
                                     style={'max-width': '250px'}
                                 )
@@ -169,44 +180,57 @@ app.layout = html.Div(
                     ]
                 ),
                 html.Div(
+                    id="div-header-4",
                     className="three columns div-user-controls",
                     children=[
                         "Percentage of listings: ",
-                        html.P(id="percentage-listings", style={"height": "35px"}),
+                        html.P(id="percentage-listings", style={"height": "35px", "font-size": "12"}),
                         "Rank of location: ",
                         html.P(id="rank-location", style={"height": "35px"}),
-                    ]
+                    ]  # TODO: Mudar aspeto deste output. Mudar tamanho de letra, etc.
                 )
             ]
         ),
         html.Div(
+            id="div-data",
             children=[
                 html.Div(
                     # MAP
+                    id="div-map-graph",
                     className="eight columns",
                     children=[
                         dcc.Graph(figure=fig_map, id="dcc_map_graph")
-                    ]
+                    ]  # TODO: escala, titulo, norte, etc.
                 ),
                 html.Div(
                     # GRAPHS
+                    id="div-other-graphs",
                     className="four columns scrollcol bg-grey",
                     children=[
                         dcc.Graph(figure=fig_pie, id="dcc_pie_graph"),
                         dcc.Graph(figure=fig_bar, id="dcc_bar_graph"),
                         dcc.Graph(figure=fig_hist, id="dcc_hist_graph")
-                    ]
+                    ]  # TODO: meter scroll bar
                 )
             ]
         )
     ]
-)
+)  # TODO: Fixar dashboard para nao rodar na vertical
 
 # --------------------------------------------------- CALLBACKS
+# number of obs to calculate percent listings
+nobs = df.shape[0]
+
+# ranking of municipalities
+location_ranking = df[["review_scores_location", "neighbourhood_group_cleansed"]]\
+    .groupby("neighbourhood_group_cleansed").mean().\
+    sort_values(by="review_scores_location", ascending=False).index.tolist()
+
 rates = list(df.ordinal_rating.unique())
 neig = list(df.neighbourhood_group_cleansed.unique())
 price = [[df.price.min(), df.price.max()]]
 room = list(df.room_type.unique())
+
 
 def slice_df(neig=neig, rates=rates, price=price, room=room):
     aux = df.copy()
@@ -219,6 +243,7 @@ def slice_df(neig=neig, rates=rates, price=price, room=room):
     # slice price
     aux = aux.loc[(aux['price'] > price[0]) & (aux['price'] < price[-1])]
     return aux
+
 
 list_of_neighbourhoods = {
     "All": {"lat": 39, "lon": -9.2, "zoom": 8.5},
@@ -278,6 +303,31 @@ def update_graph(selectedlocation, selectedvariable):
             )
         )
     )
+
+# Update the percentage of listings according to neighbourhood
+@app.callback(
+    Output("percentage-listings", "children"),
+    [
+        Input("dcc_neighbourhood_dropdown", "value")  # TODO: Este input está errado. Tem de ser filtros aos dados
+    ]
+)
+def update_perc_listings(neighbpicked):
+    return "Percentage of Listings: {:,d}".format(
+        df.loc[df["neighbourhood_group_cleansed"] == neighbpicked].shape[0] / nobs
+    )
+
+# Update the rank of location according to neighbourhood
+@app.callback(
+    Output("rank-location", "children"),
+    [
+        Input("dcc_neighbourhood_dropdown", "value")
+    ]
+)
+def update_rank_municip(neighbpicked):
+    return "#{}".format(
+        location_ranking.index(neighbpicked)+1
+    )
+
 
 if __name__ == '__main__':
     app.run_server()
